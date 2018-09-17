75f45887ee2fe3bd5745ebfa34dc9d82ec594bff
Tassilo Vermeulen <github@tassilo.nu>
2018-09-17 13:38:01 +0200

    initial version

---
+++ Display/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+# Create your views here.
+from django.views import View
+
+from nsapi import NSApi
+
+
+class IndexView(View):
+    def get(self, request):
+        apiwrapper = NSApi("527188@student.glu.nl", "pd6lcTRQk3UXIy5KgRUi1qR6gCBCeBZuocc4vuNajaKi6I9G2D6gmQ")
+        gutt = apiwrapper.get_departures("ut")
+
+        for train in gutt:
+            gutt[train]["departure_time"] = gutt[train]["departure_time"].strftime("%H:%M")
+            gutt[train]["comments"] = "\n".join(gutt[train]["comments"])
+            if gutt[train]["route"]:
+                gutt[train]["via_text"] = "via {}".format(gutt[train]["route"])
+            else:
+                gutt[train]["via_text"] = ""
+
+        return render(request, "Display/home.html", {
+            "gutt": gutt
+        })
