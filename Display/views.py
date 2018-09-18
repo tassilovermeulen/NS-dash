b329d2c0e134e17a1262ba347268475b7af6768f
Hans Goor <RootedDev@outlook.com>
2018-09-18 09:38:44 +0200

    It works now

--- Display/views.py
+++ Display/views.py
@@ -7,9 +7,13 @@ from nsapi import NSApi
 
 class IndexView(View):
     def get(self, request):
+        # Create the API wrapper
         apiwrapper = NSApi("527188@student.glu.nl", "pd6lcTRQk3UXIy5KgRUi1qR6gCBCeBZuocc4vuNajaKi6I9G2D6gmQ")
+
+        # Get the departures
         gutt = apiwrapper.get_departures("ut")
 
+        # Edit some information
         for train in gutt:
             gutt[train]["departure_time"] = gutt[train]["departure_time"].strftime("%H:%M")
             gutt[train]["comments"] = "\n".join(gutt[train]["comments"])
@@ -18,16 +22,11 @@ class IndexView(View):
             else:
                 gutt[train]["via_text"] = ""
 
-        return render(request, "Display/home.html", {
-            "gutt": gutt
-        })
-
-    def get(self, request):
-        apiwrapper = NSApi("527188@student.glu.nl", "pd6lcTRQk3UXIy5KgRUi1qR6gCBCeBZuocc4vuNajaKi6I9G2D6gmQ")
+        # Get the disruptions
         badd = apiwrapper.get_disruptions(actual=True)
 
+        # Render the HTML template
         return render(request, "Display/home.html", {
+            "gutt": gutt,
             "badd": badd
         })
-
-
